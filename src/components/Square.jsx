6898f858e9a832c9659470bafd57c26d17d96ddf
zinminht <xioniven@gmail.com>
2023-02-16 20:01:38 +0630

    16.02.2023

---
+++ src/components/Square.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../App.css';
+
+export const Square = ({val, chooseSquare}) => {
+    return (
+        <div className="square" onClick={chooseSquare}>
+            {val}
+        </div>
+    )
+}
